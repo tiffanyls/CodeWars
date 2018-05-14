9788bf9092fa3ed754c6a2f3d04763d723546a3e
Tiffany Schureman <tschureman@gmail.com>
2018-05-13 22:00:08 -0500

    another kata done

--- Codewars.js
+++ Codewars.js
@@ -102,3 +102,17 @@ function paperwork(n, m) {
    return n * m;
   }
 }
+
+//Define a function that removes duplicates from an array of numbers and returns it as a result.
+
+//The order of the sequence has to stay the same.
+
+function distinct(a) {
+  let newArr = [];
+  for (i=0; i < a.length; i++){
+    if(newArr.indexOf(a[i]) == -1){
+    newArr.push(a[i])
+    }
+    }
+    return newArr;
+  }
