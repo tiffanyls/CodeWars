cdd1ab16788fb9247943936ed7ee7a308a7a5e73
Tiffany Schureman <tschureman@gmail.com>
2018-05-23 20:00:44 -0500

    another kata done

--- Codewars.js
+++ Codewars.js
@@ -116,3 +116,18 @@ function distinct(a) {
     }
     return newArr;
   }
+
+//   It's the academic year's end, fateful moment of your school report. The averages must be calculated. All the students come to you and entreat you to calculate their average for them. Easy ! You just need to write a script.
+
+// Return the average of the given array rounded down to its nearest integer.
+
+// The array will never be empty.
+
+function getAverage(marks){
+  var total = 0;
+  for(i=0; i< marks.length; i ++){
+   total += marks[i];
+ }
+ 
+ return Math.floor(total / marks.length);
+ }
