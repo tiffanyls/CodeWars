fd7a731b8f3f2f49e8326957f5e980db728e7e03
Tiffany Schureman <tschureman@gmail.com>
2018-05-25 22:14:25 -0500

    another kata

--- Codewars.js
+++ Codewars.js
@@ -131,3 +131,13 @@ function getAverage(marks){
  
  return Math.floor(total / marks.length);
  }
+
+ // Given a number, 3 for example, return a string with a murmur: "1 sheep...2 sheep...3 sheep..."
+
+ var countSheep = function (num){
+  var sheep = '';
+    for (var i = 1; i <= num; i++) {
+      sheep += i + ' sheep...'
+    }
+   return sheep;
+  }
